a445f18e456dad6104be82c510c223460df89b99
SunRay27 <pirotexnic27@gmail.com>
2019-03-27 02:12:53 +0300

    -initial

---
+++ js/smooth-scroll.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "" && this.href.includes(window.location.href.split("#")[0])) 
+    {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+   if (!$('html, body').is(':animated'))
+   {
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 500, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }
+
+    } // End if
+  });
+});
+
+
+function ScrollToAnchor (hash)
+{
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 600, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+
+}
+
+
